fe742615da5f29f1af5b2272691e4ff9f3ec8b1f
Natalia <natalia_maltseva@bk.ru>
2019-05-24 23:24:42 +0300

    сортировки

---
+++ Merge/cmake-build-debug/CMakeFiles/Merge.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Merge.cpp.dir/main.cpp.obj"
+  "Merge.cpp.pdb"
+  "Merge.cpp.exe"
+  "Merge.cpp.exe.manifest"
+  "libMerge.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Merge.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Merge/cmake-build-debug/CMakeFiles/Merge.h.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Merge.h.dir/main.cpp.obj"
+  "CMakeFiles/Merge.h.dir/Merge.cpp.obj"
+  "Merge.h.pdb"
+  "Merge.h.exe"
+  "Merge.h.exe.manifest"
+  "libMerge.h.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Merge.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

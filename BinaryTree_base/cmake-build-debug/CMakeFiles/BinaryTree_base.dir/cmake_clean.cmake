4c1ce287475f38d8085282af6b378ae53dcd0e7e
Natalia <natalia_maltseva@bk.ru>
2019-05-24 23:22:40 +0300

    наследование бинарного и КЧ дерева от базового

---
+++ BinaryTree_base/cmake-build-debug/CMakeFiles/BinaryTree_base.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BinaryTree_base.dir/main.cpp.obj"
+  "CMakeFiles/BinaryTree_base.dir/binary_tree.cpp.obj"
+  "CMakeFiles/BinaryTree_base.dir/base_tree.cpp.obj"
+  "CMakeFiles/BinaryTree_base.dir/RB_tree.cpp.obj"
+  "BinaryTree_base.pdb"
+  "BinaryTree_base.exe"
+  "BinaryTree_base.exe.manifest"
+  "libBinaryTree_base.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinaryTree_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
